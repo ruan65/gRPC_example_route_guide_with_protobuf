a0aa3433ff7c9ac2b25d021ffd4eddb2377ca928
a <ruan65@gmail.com>
2019-06-01 19:11:48 +0300

    initial

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:route_guide_google_example/route_guide_google_example.dart' as route_guide_google_example;
+
+main(List<String> arguments) {
+  print('Hello world: ${route_guide_google_example.calculate()}!');
+}
---
+++ test/route_guide_google_example_test.dart
@@ -0,0 +1,8 @@
+import 'package:route_guide_google_example/route_guide_google_example.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
